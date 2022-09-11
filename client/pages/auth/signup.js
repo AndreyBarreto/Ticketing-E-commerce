6e67998ea4b002de9b9f88f1da4c8bfebb6530dd
Andrey Barreto <dey@LAPTOP-RVOBPJ62.localdomain>
2022-09-11 11:29:02 -0300

    feature/singup page next

---
+++ client/pages/auth/signup.js
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import useRequest from "../../hooks/use-request";
+import Router from "next/router";
+
+const Landing = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { doRequest, errors } = useRequest({
+    url: "/api/users/signup",
+    method: "post",
+    body: {
+      email,
+      password,
+    },
+    onSuccess: () => Router.push("/"),
+  });
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    await doRequest();
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <h1>Sign Up</h1>
+      <div className="form-group">
+        <label>Email address</label>
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="form-control"
+        ></input>
+      </div>
+      <div className="form-group">
+        <label>Password</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="form-control"
+        ></input>
+      </div>
+      {errors}
+      <button className="btn btn-primary">Sign up</button>
+    </form>
+  );
+};
+
+export default Landing;
