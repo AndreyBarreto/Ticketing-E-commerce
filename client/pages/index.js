0be86c43f190eed39b04ca9079595fb18637652a
Andrey Barreto <dey@LAPTOP-RVOBPJ62.localdomain>
2022-09-14 19:06:25 -0300

    feature/header & and sign out

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,19 @@
-const Landing = () => {
-  return <h1>Landing Page222</h1>;
+import buildClient from "../api/build-client";
+
+const Landing = ({ currentUser }) => {
+  return currentUser ? (
+    <h1>You are signed in</h1>
+  ) : (
+    <h1>You are not signed in</h1>
+  );
+};
+
+Landing.getInitialProps = async (context) => {
+  const client = buildClient(context);
+
+  const { data } = await client.get("/api/users/currentuser");
+
+  return data;
 };
 
 export default Landing;
